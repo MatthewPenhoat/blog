6f905b1f69189a260f2d80bff78cd6dd4bf279b1
Matthew Penhoat <matthewpenhoat@outlook.com>
2020-10-27 15:57:54 +0100

    blog.php added + background image

---
+++ blog.php
@@ -0,0 +1,25 @@
+<?php include "include/header.php";
+?>
+
+<div class="header">
+  <div class="container">
+    <h1>blog</h1>
+  </div>
+</div>
+
+<div class="navigation">
+  <div class="container">
+    <a href="/">Home</a>
+    <a href="/blog.php">Blog</a>
+    <a href="/about.php">Over ons</a>
+    <a href="/contact.php">Contact</a>
+    <div class="clearfix"></div>
+  </div>
+</div>
+
+
+
+
+
+
+
