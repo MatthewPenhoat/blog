4838e5a8c5f8201d895f7b8c54409b3522ee9e75
Matthew Penhoat <matthewpenhoat@outlook.com>
2020-10-30 16:38:16 +0100

    fout melding toegevoegd + titel site

--- include/header.php
+++ include/header.php
@@ -1,5 +1,11 @@
+<?php
+  ini_set('display_startup_errors', 1);
+  ini_set('display_errors', 1);
+  error_reporting(-1);
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="nl">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,7 +13,7 @@
   <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@1,700&display=swap" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&display=swap" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Bungee+Shade&display=swap" rel="stylesheet">
-  <title>Document</title>
+  <title>Matthew Penhoat</title>
   <link rel="stylesheet" href="/assets/style.css?v=<?php echo time(); ?>">
 </head>
 <body>
